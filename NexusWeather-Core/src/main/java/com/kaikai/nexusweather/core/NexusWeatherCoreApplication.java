b31f62b4bebde255ed75cd8c0c8fbf07cb409597
Catkaikai <2289751443@qq.com>
2021-12-10 23:28:51 +0800

    init project

---
+++ NexusWeather-Core/src/main/java/com/kaikai/nexusweather/core/NexusWeatherCoreApplication.java
@@ -0,0 +1,13 @@
+package com.kaikai.nexusweather.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusWeatherCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusWeatherCoreApplication.class, args);
+    }
+
+}
---
+++ NexusWeather-Core/src/test/java/com/kaikai/nexusweather/core/NexusWeatherCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaikai.nexusweather.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusWeatherCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
